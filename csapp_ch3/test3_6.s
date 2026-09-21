3d4de7d848582fccea4d1fa4c3991d15f3aed7d9
YoonJinhoon <https://github.com/YoonJinhoon/debugging_lab_docker>
2026-09-21 07:50:43 +0000

    doc(csapp): add -O0 control flow execution diagram

---
+++ csapp_ch3/test3_6.s
@@ -0,0 +1,47 @@
+	.file	"test3_6.c"
+	.text
+	.globl	max
+	.type	max, @function
+max:
+.LFB0:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movq	%rdi, -8(%rbp)
+	movq	%rsi, -16(%rbp)
+	movq	-8(%rbp), %rax
+	cmpq	-16(%rbp), %rax
+	jle	.L2
+	movq	-8(%rbp), %rax
+	jmp	.L3
+.L2:
+	movq	-16(%rbp), %rax
+.L3:
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	max, .-max
+	.ident	"GCC: (Ubuntu 13.3.0-6ubuntu2~24.04.1) 13.3.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	1f - 0f
+	.long	4f - 1f
+	.long	5
+0:
+	.string	"GNU"
+1:
+	.align 8
+	.long	0xc0000002
+	.long	3f - 2f
+2:
+	.long	0x3
+3:
+	.align 8
+4:
